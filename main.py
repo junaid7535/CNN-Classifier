46022bad8e0b0f8c745e44c5e1885b3d67ad5cca
junaid7535 <zunaidkhan1492@gmail.com>
2024-11-09 16:43:41 +0530

    mode training

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src import logger
 from src.pipeline.stage_1_data_ingestion import DataIngestionTrainingPipeline
 from src.pipeline.stage_2_prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.pipeline.stage_3_model_training import ModelTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -26,3 +28,14 @@ except Exception as e:
 
 
 
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
